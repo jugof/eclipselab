5e2be16d7231d8ad069f22c5eed841621fcbc8a3
jugof <jugisoff@gmail.com>
2024-05-25 23:37:29 +0530

    commit

--- Spring_Beans_Project/src/main/java/example/hibernate/entity/SpringExampleMain.java
+++ Spring_Beans_Project/src/main/java/example/hibernate/entity/SpringExampleMain.java
@@ -14,13 +14,26 @@ public class SpringExampleMain {
 		
 		Object loadedObj = ctx.getBean("st1");
 		Stock s = (Stock)loadedObj;
+		s.setName("Lenovo");
+		s.setStockPrice(1000);
+		s.setStockId(301);
 		
 		System.out.println(s.toString());
 		
 		Object loaded = ctx.getBean("st2");
 		StockHolder sh = (StockHolder)loaded;
+		sh.setAccountNo(6201);
+		sh.setName("Aatif");
+		sh.setEmail("aatif@gmail.com");
 		
 		System.out.println(sh.toString());
+		
+		Object loadedObject = ctx.getBean("st3");
+		Student std = (Student)loadedObject;
+		std.setDegree("MBBS-[Neurologist]");
+		std.setName("Dr. Tejal Salunke");
+		std.setStudentId(120);
+		System.out.println(std.toString());
 	}
 
 }
