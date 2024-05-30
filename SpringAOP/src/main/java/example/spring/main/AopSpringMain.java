56523d30d45a2a4f0cefa1096b450fee5a46d74e
jugof <jugisoff@gmail.com>
2024-05-30 16:15:48 +0530

    lab

---
+++ SpringAOP/src/main/java/example/spring/main/AopSpringMain.java
@@ -0,0 +1,11 @@
+package example.spring.main;
+
+public class AopSpringMain {
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- SpringAOP/src/main/java/spring/annotation/config/SpringConfig.java
+++ SpringAOP/src/main/java/spring/annotation/config/SpringConfig.java
@@ -1,5 +1,6 @@
 package spring.annotation.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import example.spring.aop.Stock;
@@ -7,6 +8,7 @@ import example.spring.aop.Stock;
 @Configuration
 public class SpringConfig {
 	
+	@Bean
 	public Stock createStock() {
 		return new Stock(101, "SpaceX", 1200);
 	}
